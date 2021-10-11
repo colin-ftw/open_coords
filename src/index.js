ad7ca2a413f495520cc61ed225597411b51808ba
Colin <colinraney@gmail.com>
2021-10-11 17:30:33 -0400

    updating to update webflow

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles'
+import './scripts'
+import IndexView from './views/IndexView'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexView />
   </React.StrictMode>,
   document.getElementById('root')
 );
